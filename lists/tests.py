c645301150139467c4e532d5cf55cd3279641e76
Lukas Halbritter <halbi93@gmx.de>
2023-05-12 13:10:13 +0200

    Added the concept of lists.

--- lists/tests.py
+++ lists/tests.py
@@ -3,22 +3,30 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -27,21 +35,30 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
-    def test_uses_home_template(self):
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
         self.assertTemplateUsed(response, "list.html")
 
-    def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=correct_list)
+        Item.objects.create(text="itemey 2", list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text="other list item 1", list=other_list)
+        Item.objects.create(text="other list item 2", list=other_list)
 
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+        response = self.client.get(f"/lists/{correct_list.id}/")
 
         self.assertContains(response,"itemey 1")
         self.assertContains(response,"itemey 2")
+        self.assertNotContains(response,"other list item 1")
+        self.assertNotContains(response,"other list item 2")
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -54,4 +71,5 @@ class NewListTest(TestCase):
         response = self.client.post(
             "/lists/new",
             data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+        new_list = List.objects.first()
+        self.assertRedirects(response, f"/lists/{new_list.id}/")
